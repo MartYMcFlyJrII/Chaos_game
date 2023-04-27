c04baf727b08461790455d5c55be666d14097fed
David Nuñez <davidnunez635@gmail.com>
2023-04-27 12:38:46 -0700

    testing and improvements

--- Chaos_game.py
+++ Chaos_game.py
@@ -3,9 +3,13 @@ import random
 import math
 import colorsys
 from pygame.locals import *
-
+from enum import Enum
 index = [0, 0, 0]
 points=[]
+
+class Mode(Enum):
+    AUTOMATIC = 0
+    MANUAL = 1
 
 def mark_pixel(surface, pos, pcol):
     col = surface.get_at(pos)
@@ -23,79 +27,85 @@ def random_point_index(p):
     index[1] = index[0]
     dst1 = abs(index[1] - index[2])
 
+    used_indices = set(index)
     while True:
         index[0] = random.randint(0, len(p) - 1)
         dst = abs(index[0] - index[1])
         if dst1 == 0 and (dst == 1 or dst == len(p) - 1):
             continue
+        elif index[0] in used_indices:
+            continue
         else:
+            used_indices.add(index[0])
             break
 
     return index[0]
 
 
-def init_polygon(width, height, n, mode):
+def init_polygon(width, height, n=3, mode=Mode.AUTOMATIC):
     
     delta_angle = 360/n
     r = width/2 - 10
     p = []
 
-    for i in range(0, len(points)):
+    for i in range(0, n):
         angle = (180 + i*delta_angle) * math.pi / 180
         color = colorsys.hsv_to_rgb((i*delta_angle)/360, 0.8, 1)
         # with a regular polygon
-        if mode==0:
+        if mode==Mode.AUTOMATIC:
             
             p.append(((width/2 + r*math.sin(angle),
                     height/2 + r*math.cos(angle)),
                     (int(color[0]*255), int(color[1]*255), int(color[2]*255))))
         # manual points
-        elif mode==1:
+        elif mode==Mode.MANUAL:
             p.append(((points[i][0],
                     points[i][1]),
                     (int(color[0]*255), int(color[1]*255), int(color[2]*255))))
-        else:
-            print("modo no valido, por favor ingrese 0 para automatico y 1 para manual")
-            exit()
 
     print(p)
     return p
 
 def main(width, height, n, r,mode):
-    pygame.init()
-    surface = pygame.display.set_mode((width, height))
-    pygame.display.set_caption('Chaos Game')
-    x, y = (400, 300)
-    step = 0
-    points_clicked = 0
-    global points
-    while True:
-        
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                pygame.image.save(surface, 'chaosspiel.jpg')
-                pygame.quit()
-                return
-            elif event.type == MOUSEBUTTONDOWN:
-                if points_clicked < n:
-                    pygame.draw.circle(surface, (0, 0, 255), event.pos, 1 )
-                    points_clicked += 1                    
-                    points.append((event.pos[0],event.pos[1]))
+    try:
+        Mode(mode)
+        pygame.init()
+        surface = pygame.display.set_mode((width, height))
+        pygame.display.set_caption('Chaos Game')
+        x, y = (400, 300)
+        step = 0
+        points_clicked = 0
+        global points
+        while True:
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.image.save(surface, 'chaos_game_example.jpg')
+                    pygame.quit()
+                    return
+                elif event.type == MOUSEBUTTONDOWN and mode==Mode.MANUAL:
+                    if points_clicked < n:
+                        pygame.draw.circle(surface, (0, 0, 255), event.pos, 1 )
+                        points_clicked += 1                    
+                        points.append((event.pos[0],event.pos[1]))
+                        pygame.display.update()
+            if points_clicked == n or mode==Mode.AUTOMATIC:
+                
+                step = step + 1
+                p = init_polygon(width, height, n,mode)
+                point_idx = random_point_index(p)
+                pos = p[point_idx][0]
+                color = p[point_idx][1]
+                x += (pos[0] - x) * r
+                y += (pos[1] - y) * r
+
+                mark_pixel(surface, (int(x), int(y)), color)
+
+                if step % 1000 == 0:
                     pygame.display.update()
-        if points_clicked == n:
-            step = step + 1
-            p = init_polygon(width, height, n,mode)
-            point_idx = random_point_index(p)
-            pos = p[point_idx][0]
-            color = p[point_idx][1]
-            x += (pos[0] - x) * r
-            y += (pos[1] - y) * r
-
-            mark_pixel(surface, (int(x), int(y)), color)
-
-            if step % 1000 == 0:
-                pygame.display.update()
+    except ValueError:
+        print("Valor de modo invalido. Por favor ingresar 0 por modo automatico y 1 para manual")
 
 
 if __name__ == "__main__":
-    n=5;mode=0; main(800, 800, n, 0.45, mode)
+    n=3;mode=5; main(800, 800, n, 0.45, mode)
+    
